a785dfa4c2e67628d865332f95720c2192a16ede
Victor Lafargue <lafarg_b@r06p09.cisco.sm.cri.epita.net>
2017-05-06 17:52:35 +0200

    Draw Grid

--- src/main/java/MapPanelView.java
+++ src/main/java/MapPanelView.java
@@ -1,6 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -35,18 +36,25 @@ public class MapPanelView extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        int x = (gridDisplay ? 1 : 0);
-        int y = x;
-
-        int offset = 16 + (gridDisplay ? 1 : 0);
+        int x = 0;
+        int y = 0;
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 g.drawImage(defaultTile, x, y, null);
-                x += offset;
+                x += 16;
             }
-            x = (gridDisplay ? 1 : 0);
-            y += offset;
+            x = 0;
+            y += 16;
+        }
+
+        if (gridDisplay) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(new Color(0, 0, 0, 100));
+            for (x = 16; x < width * 16; x += 16)
+                g2.draw(new Line2D.Float(x, 0, x, height * 16));
+            for (y = 16; y < height * 16; y += 16)
+                g2.draw(new Line2D.Float(0, y, width * 16, y));
         }
     }
 }
