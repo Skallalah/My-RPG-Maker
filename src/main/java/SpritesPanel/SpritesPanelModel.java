a559f2aef00e033444c0760d61b9480d73b2c242
wxbn <wxbn@live.fr>
2017-05-08 16:43:15 +0200

    Removed image

--- src/main/java/SpritesPanel/SpritesPanelModel.java
+++ src/main/java/SpritesPanel/SpritesPanelModel.java
@@ -19,7 +19,6 @@ public class SpritesPanelModel {
     public void setAsSelected(String selectedSprite) {
         SpriteResources.selectedSprite = selectedSprite;
         Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Image image = SpriteResources.pathToImage.get(selectedSprite);
         BufferedImage img = SpriteResources.pathToImage.get(selectedSprite);
         Cursor c = toolkit.createCustomCursor(img, new Point(img.getWidth() / 2, img.getHeight() / 2), "img");
         editorWindow.setCursor(c);
